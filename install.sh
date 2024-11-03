48f5beebac75acb94c1a90248ebb4ca90d8d0fa8
Sebastian Noack <sebastian.noack@gmail.com>
2024-11-02 20:22:46 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -3,45 +3,41 @@
 set -eu
 
 NAME=ekm-meter-watcher
-USER=pi
-CWD=$(eval echo ~$USER/$NAME)
-SCRIPT=/usr/local/bin/$NAME
-SCRIPT_URL=https://raw.githubusercontent.com/snoack/$NAME/main/$NAME.py
+CWD=~/$NAME
+SCRIPT=$NAME.py
+SCRIPT_URL=https://raw.githubusercontent.com/snoack/$NAME/main/$SCRIPT
 CRON_SCRIPT=/etc/cron.daily/$NAME
-WATCHER_LOG=$CWD/watcher.log
-CRON_LOG=$CWD/cron.log
+WATCHER_LOG=watcher.log
+CRON_LOG=cron.log
 
 echo "Installing dependencies..."
 sudo apt-get install -y pigpio python3-pigpio sqlite3
 
-echo "Downloading script..."
-sudo curl --silent --show-error $SCRIPT_URL -o $SCRIPT
-sudo chmod +x $SCRIPT
-
 echo "Creating $CWD directory..."
-sudo su $USER -c "mkdir -p $CWD"
+mkdir -p $CWD
+
+echo "Downloading script..."
+curl --silent --show-error $SCRIPT_URL -o $CWD/$SCRIPT
+chmod +x $CWD/$SCRIPT
 
 echo "Setting up cronjob..."
-sudo -s << EOS
-cat << EOF > $CRON_SCRIPT
-#!/bin/sh
-su $USER -c 'cd $CWD && $NAME --aggregate' > $CRON_LOG 2>&1
+crontab - << EOF
+$(crontab -l 2>/dev/null | grep -v $NAME || true)
+25 6	* * *	cd $CWD && ./$SCRIPT --aggregate > $CRON_LOG 2>&1
 EOF
-EOS
-sudo chmod +x $CRON_SCRIPT
 
 echo "Setting up systemd service..."
 sudo -s << EOS
-cat << EOF > /lib/systemd/system/$NAME.service
+cat << EOF > /etc/systemd/system/$NAME.service
 [Unit]
 Description=Script that records impulses from EKM meter via GPIO
 Requires=pigpiod.service
 [Service]
 User=$USER
 WorkingDirectory=$CWD
-StandardOutput=file:$WATCHER_LOG
-StandardError=file:$WATCHER_LOG
-ExecStart=$SCRIPT
+StandardOutput=file:$CWD/$WATCHER_LOG
+StandardError=file:$CWD/$WATCHER_LOG
+ExecStart=$CWD/$SCRIPT
 [Install]
 WantedBy=multi-user.target
 EOF
